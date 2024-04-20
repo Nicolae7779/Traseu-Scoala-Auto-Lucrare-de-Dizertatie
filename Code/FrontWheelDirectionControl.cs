3e9b7fe5614e733b479d10c38f3a056b200a867b
FluffyOverload <159545712+Nicolae7779@users.noreply.github.com>
2024-04-20 22:19:27 +0300

    Update FrontWheelDirectionControl.cs

--- Code/FrontWheelDirectionControl.cs
+++ Code/FrontWheelDirectionControl.cs
@@ -1,1 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
+public class RotireRoti : MonoBehaviour
+{
+
+    LogitechGSDK.LogiControllerPropertiesData proprieties;
+
+    public float maxSteeringAngle;
+    private float steerAngle;
+    public float xAxes;
+
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+
+    void Start()
+    {
+        print(LogitechGSDK.LogiSteeringInitialize(false));
+    }
+ 
+    private void FixedUpdate()
+    {
+
+        if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
+         {
+             LogitechGSDK.DIJOYSTATE2ENGINES rec;
+             rec = LogitechGSDK.LogiGetStateUnity(0);
+
+            xAxes = rec.lX / 32768f;
+            steerAngle = maxSteeringAngle * xAxes;
+        }
+
+         frontLeftWheelCollider.steerAngle = steerAngle;
+         frontRightWheelCollider.steerAngle = steerAngle;
+
+
+        UpdateWheelPos(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheelPos(frontRightWheelCollider, frontRightWheelTransform);
+
+
+        void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
+         {
+              Vector3 pos;
+              Quaternion rot;
+              wheelCollider.GetWorldPose(out pos, out rot);
+              trans.rotation = rot;
+              trans.position = pos;
+         }
+    }
+}
